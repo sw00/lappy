4d48ecc59001e4629d8fde609cba8a490a9d31a8
sw00 <sett.wai@gmail.com>
2017-06-10 19:42:24 +0200

    Escalate to sudo in linux bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,12 @@ OS=$(uname -s)
 if [[ $OS = 'Darwin' ]]; then
     curl -sSL https://raw.githubusercontent.com/sw00/lappy/master/scripts/osx.sh | sh
 elif [[ $OS = 'Linux' ]]; then
+
+    if [ $EUID != 0 ]; then
+	sudo "$0" "$@"
+	exit $?
+    fi
+
     curl -sSL https://raw.githubusercontent.com/sw00/lappy/master/scripts/linux.sh | sh
 fi
 
