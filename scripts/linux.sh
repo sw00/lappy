0db9c7777302b4e6bfef61fa46ed4b652c9cab52
sw00 <sett.wai@gmail.com>
2017-06-10 19:23:35 +0200

    Add linux provision scripts

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,10 +3,14 @@
 DISTRO=$(uname -r | tail -c 5)
 
 if [[ $DISTRO = 'ARCH' ]]; then
-    pacman -S --noconfirm git python 
+    pacman -Sy --noconfirm git ansible 
 
-    git clone https://github.com/sw00/lappy.git /tmp/lappy
+    git clone \
+	--recurse-submodules \
+	--shallow-submodules \
+	https://github.com/sw00/lappy.git /tmp/lappy
 
+    export ANSIBLE_NOCOWS=1
     cd /tmp/lappy && \
 	ansible-playbook -vv site.yml
 fi
